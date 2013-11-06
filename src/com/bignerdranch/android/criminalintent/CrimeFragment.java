109c5b8292d109f29a5264f08d14c1be719e0d91
Matthew Farley <matfarley@gmail.com>
2013-11-07 10:34:55 +1300

    Chapter16

--- src/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ src/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -3,14 +3,18 @@ package com.bignerdranch.android.criminalintent;
 import java.util.Date;
 import java.util.UUID;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.NavUtils;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -47,6 +51,7 @@ public class CrimeFragment extends Fragment {
 		super.onCreate(savedInstanceState);
 		UUID crimeId= (UUID)getArguments().getSerializable(EXTRA_CRIME_ID);
 		mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
+		setHasOptionsMenu(true);
 	}
 	
 	private void updateDate(){
@@ -54,8 +59,28 @@ public class CrimeFragment extends Fragment {
 	}
 	
 	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+		switch (item.getItemId()){
+		case android.R.id.home:
+			if(NavUtils.getParentActivityName(getActivity()) != null){
+				NavUtils.navigateUpFromSameTask(getActivity());
+			}
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+	
+	@TargetApi(11)
+	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState){
 		View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
+			if(NavUtils.getParentActivityName(getActivity()) != null){
+			    getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+			}
+		}
+		
 		//Assign widgets etc after the view is instantiated.
 		mTitleField = (EditText)v.findViewById(R.id.crime_title);
 		mTitleField.setText(mCrime.getTitle());
--- src/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ src/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -10,16 +10,10 @@ public class CrimeLab {
 	private Context mAppContext;
 	private ArrayList<Crime> mCrimes;
 	
-	private CrimeLab(Context appContext){ //Private constructor for the singleton - only accessble from this class
+	private CrimeLab(Context appContext){ //Private constructor for the singleton - only accessible from this class
 		mAppContext = appContext;
 		mCrimes = new ArrayList<Crime>();
-		//Initial Crimes for Testing
-		for(int i = 0; i < 100; i++){
-			Crime c = new Crime();
-			c.setTitle("Crime #" + i);
-			c.setSolved(i % 2 == 0); //the result will be a boolean. Every 2nd iteration will evenly divide by 2
-			mCrimes.add(c);
-		}
+		
 	}
 	
 	public static CrimeLab get(Context c){ //Only way to instantiate the singleton from outside. Can only ever be one instance.
@@ -27,6 +21,10 @@ public class CrimeLab {
 			sCrimeLab = new CrimeLab(c.getApplicationContext());
 		}
 		return sCrimeLab;
+	}
+	
+	public void addCrime(Crime c){
+		mCrimes.add(c);
 	}
 	
 	public ArrayList<Crime> getCrimes(){
